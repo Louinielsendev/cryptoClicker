0d35bd11f19b6d3a003f3e8683fbbd64c6e23343
Loui Lindkvist Nielsen <loui.apa@gmail.com>
2023-01-30 18:19:51 +0100

    touch

--- Main.js
+++ Main.js
@@ -1,12 +1,30 @@
+var coin 
 Main = {
-    init : function (){
+    init: function () {
         console.log('init')
+        coin = document.querySelector('.coin')
+        coin.addEventListener('touchstart', function(e){
+            scaleCoin(e)
+        })
+        coin.addEventListener('touchend', function(e){
+            scaleCoin(e)
+        })
+    
     },
-    animate : function () {
-       // var animation = window.requestAnimationFrame(Main.animate)
+    animate: function () {
+        // var animation = window.requestAnimationFrame(Main.animate)
         console.log('ani')
     }
 }
 
-window.addEventListener('load', Main.init); 
+
+function scaleCoin(event) {
+    
+    event.type === 'touchstart' ?
+    coin.style.transform = 'scale(.9)' :
+    coin.style.transform = 'scale(1)'
+}
+
+
+window.addEventListener('load', Main.init);
 window.addEventListener('load', Main.animate); 
