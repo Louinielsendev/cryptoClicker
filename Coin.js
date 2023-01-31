85d2f21aa06f39f3545dd72278bc199f7b450b7d
Loui Lindkvist Nielsen <ll223vk@student.lnu.se>
2023-01-31 17:40:43 +0100

    touch only

--- Coin.js
+++ Coin.js
@@ -6,29 +6,28 @@ var Coin = function () {
 
     this.image.addEventListener('touchstart', () => { this.clickDown() })
     this.image.addEventListener('touchend', () => { this.clickUp() })
-    this.image.addEventListener('mousedown', () => { this.clickDown() })
-    this.image.addEventListener('mouseup', () => { this.clickUp() })
 }
 
 Coin.prototype.clickDown = function () {
     this.image.style.transform = 'scale(.95)'
-    const randomnumber = Math.random() * 400
-    var drop = new Drop({position: {
-        x: randomnumber,
-        y: 0
-    },
-    velocity: {
-        y: 2
-    }
-});
-    Main.drops.push(drop)
 
-    console.log(Main.drops)
+
+
 }
 
 Coin.prototype.clickUp = function () {
     this.image.style.transform = 'scale(1)'
 
-
+    const randomnumber = Math.random() * 400
+    var drop = new Drop({
+        position: {
+            x: randomnumber,
+            y: 0
+        },
+        velocity: {
+            y: 2
+        }
+    });
+    Main.drops.push(drop)
 }
 
